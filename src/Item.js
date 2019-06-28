7fb9c1149aa6f5909f88eeefe1b9baf3138e78de
Jeff Langballe <jeffreynlangballe@gmail.com>
2019-06-28 11:46:48 -0400

    Add item detail plot

--- src/Item.js
+++ src/Item.js
@@ -1,19 +1,84 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import ItemGraph from './ItemGraph';
-import {ItemsContext} from './api';
+import {ItemsContext, GetGraphData} from './api';
 
-function Item({ match }) {
-  return (
-    <ItemsContext.Consumer>
-      {({getItemProperty}) => (
-        <React.Fragment>
-          <h2>{getItemProperty(match.params.id, 'name')}</h2>
-          <ItemGraph itemId={match.params.id}></ItemGraph>
-        </React.Fragment>
-      )}
-    </ItemsContext.Consumer>
-  )
+class Item extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      range: 60*24,
+      interval: 30,
+      graphData: {},
+      showGraph: false
+    }
+  }
+
+  componentDidMount() {
+    this.loadItemData();
+  }
+
+  /**
+   * @name loadItemData
+   * @brief Get item graph data from RSBuddy api
+   */
+  async loadItemData() {
+    let itemData = await GetGraphData(this.props.match.params.id, this.state.range, this.state.interval);
+    this.setState({
+      graphData: this.parseApiGraphData(itemData),
+      showGraph: true
+    });
+  }
+
+  /**
+   * @name setGraphRange
+   * @brief Sets display range for graph
+   * @param range Fetch data from this many minutes ago until current time
+   * @param interval Interval between timestamps in minutes
+   */
+  setGraphRange(range, interval) {
+    console.log('setgraphrange', range, interval)
+    this.setState({
+      showGraph: false,
+      range: range,
+      interval: interval
+    });
+    this.loadItemData();
+  }
+
+  /**
+   * @name parseApiGraphData
+   * @brief Formats data from RSBuddy API for use in Dygraphs
+   * @param itemData RSBuddy API detailed time series data
+   * @return Object of the form {labels, values}
+   * TODO: Also include quantity data
+   */
+  parseApiGraphData(itemData) {
+    const zeroToNull = x =>  x ? x : null;
+    const labels = ['Time', 'Buy Price', 'Sell Price'];
+    const values = itemData.map(x => [new Date(x.ts), zeroToNull(x.buyingPrice), zeroToNull(x.sellingPrice)]);
+    return {labels, values};
+  }
+
+  render() {
+    return (
+      <ItemsContext.Consumer>
+        {({getItemProperty}) => (
+          <React.Fragment>
+            <h2>{getItemProperty(this.props.match.params.id, 'name')}</h2>
+            <div className="btn-group mb-1" role="group">
+              <button type="button" className="btn btn-secondary" onClick={x => this.setGraphRange(60*24*365, 17280)}>Year</button>
+              <button type="button" className="btn btn-secondary" onClick={x => this.setGraphRange(60*24*92, 4320)}>Quarter</button>
+              <button type="button" className="btn btn-secondary" onClick={x => this.setGraphRange(60*24*31, 1440)}>Month</button>
+              <button type="button" className="btn btn-secondary" onClick={x => this.setGraphRange(60*24*7, 180)}>Week</button>
+              <button type="button" className="btn btn-secondary" onClick={x => this.setGraphRange(60*24, 30)}>Day</button>
+            </div>
+            <ItemGraph graphData={this.state.graphData}/>
+          </React.Fragment>
+        )}
+      </ItemsContext.Consumer>
+    );
+  }
 }
 
 export default Item;
--- src/ItemGraph.js
+++ src/ItemGraph.js
@@ -1,40 +1,24 @@
 import React, {useRef, useEffect} from 'react';
 import Dygraph from 'dygraphs';
-import {GetGraphData} from './api';
 
-/**
- * @name parseApiGraphData
- * @brief Formats data from RSBuddy API for use in Dygraphs
- * @param itemData RSBuddy API detailed time series data
- * @return Object of the form {labels, values}
- * TODO: Also include quantity data
- */
-function parseApiGraphData(itemData) {
-  const zeroToNull = x =>  x ? x : null;
-  const labels = ['Time', 'Buy Price', 'Sell Price'];
-  const values = itemData.map(x => [new Date(x.ts), zeroToNull(x.buyingPrice), zeroToNull(x.sellingPrice)]);
-  return {labels, values};
-}
-
-// TODO: This component shouldn't be getting the data itself but should
-// instead be provided with the data in the right format
-function ItemGraph({itemId}) {
-  const graphEl = useRef(null);
-  const graphStyle = {
+class ItemGraph extends React.Component {
+  graphStyle = {
     width: '100%'
   };
-  let graphData = null;
-  let graph = null;
+  graph = null;
 
-  useEffect(() => {
-    const loadItemData = async () => {
-      let itemData = await GetGraphData(itemId, 60*24, 30); //  TODO: Don't hard-code these
-      graphData = parseApiGraphData(itemData);
-      graph = new Dygraph(
-        graphEl.current,
-        graphData.values,
+  /**
+   * @name  createGraph
+   * @brief Creates graph if data exists
+   * TODO: Calling this in two places is kind of hacky
+   */
+  createGraph() {
+    if(this.props.graphData.values) {
+      this.graph = new Dygraph(
+        this.refs.graphEl,
+        this.props.graphData.values,
         {
-          labels: graphData.labels,
+          labels: this.props.graphData.labels,
           strokeWidth: 1.5,
           drawPoints: true,
           pointSize: 2.5,
@@ -48,12 +32,18 @@ function ItemGraph({itemId}) {
           }
         });
     }
-    loadItemData();
-  }, []);
+  }
 
-  return(
-    <div ref={graphEl} style={graphStyle}></div>
-  );
+  componentDidMount() {
+    this.createGraph();
+  }
+
+  render() {
+    this.createGraph();
+    return(
+      <div ref="graphEl" style={this.graphStyle}></div>
+    );
+  }
 }
 
 export default ItemGraph;
